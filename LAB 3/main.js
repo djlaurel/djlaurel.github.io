c87ddac02ccc0e35971c1909c0a40c24a0cee1fd
djlaurel <dlaurel@torontomu.ca>
2024-02-26 15:57:33 -0500

    Lab 3

---
+++ LAB 3/main.js
@@ -0,0 +1,5 @@
+function func1() {
+    document.getElementsByClassName("menu").style.color = "dark blue";}
+        
+function func2() {
+    document.getElementsByClassName("menu").style.color = "black";}
